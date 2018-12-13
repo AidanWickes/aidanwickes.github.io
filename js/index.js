21a7c18af67814000780b194088fa13231219f7d
Aidan Wickes <aidan.wickes@gmail.com>
2018-12-13 12:48:15 +0000

    ?

--- js/index.js
+++ js/index.js
@@ -83,7 +83,7 @@ function init() {
   document.addEventListener('mousemove', onMouseMove, false);
   document.addEventListener('mousedown', onMouseDown, false);
   document.body.appendChild( WEBVR.createButton( renderer ) );
-  if (message.innerHTML == 'WEBVR NOT SUPPORTED'){
+  if (button.textContent == 'VR NOT FOUND'){
     renderer.vr.enabled = false;
   } else {
     renderer.vr.enabled = true;
